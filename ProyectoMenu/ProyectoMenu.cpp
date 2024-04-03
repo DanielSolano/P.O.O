093360b49fdf3834e34f847fa3fd251f6c13b323
Daniel Solano <solanomezaangel@gmail.com>
2024-04-03 10:18:41 -0700

    modified:   ProyectoMenu/Proyecto.exe 	modified:   ProyectoMenu/ProyectoMenu.cpp

--- ProyectoMenu/ProyectoMenu.cpp
+++ ProyectoMenu/ProyectoMenu.cpp
@@ -28,8 +28,16 @@ void ConexionesActivas()
 
 void Configuracion()
 {
-    cout << "Pendiente" << endl;
-    _getch();
+    Menu submenu("Configuracion");
+    submenu.Agregar({'1', "Subopcion 1", {[]()
+                                          {system("cls");cout << "Ejecutando subopcion 1" << endl; _getch(); }}});
+    submenu.Agregar({'2', "Subopcion 2", {[]()
+                                          {system("cls");cout << "Ejecutando subopcion 2" << endl; _getch(); }}});
+    submenu.Agregar({'3', "Subopcion 3", {[]()
+                                          {system("cls");cout << "Ejecutando subopcion 3" << endl; _getch(); }}});
+    submenu.Agregar({'4', "Regresar", {[]()
+                                       {system("cls");cout << "Regresando..." << endl; _getch(); }}});
+    submenu.Ejecutar();
 }
 
 int main()
@@ -39,7 +47,8 @@ int main()
     el_menu.Agregar({'2', "Ver configuracion IP", ConfiguracionIP});
     el_menu.Agregar({'3', "Ver conexiones activas", ConexionesActivas});
     el_menu.Agregar({'4', "Ver configuracion", Configuracion});
-    el_menu.Agregar({'5', "Salir", {[](){cout << "Saliendo..." << endl;}}});
+    el_menu.Agregar({'5', "Salir", {[]()
+                                    { cout << "Saliendo..." << endl; }}});
 
     /*
     do
@@ -49,6 +58,5 @@ int main()
     } while (el_menu.Seleccionar() != '5');
     */
 
-    el_menu.Ejecutar();   
-
+    el_menu.Ejecutar();
 }
